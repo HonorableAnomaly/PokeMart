6b9abc96332aa9e1c4e5aa6f4fcd283ada402bcc
Ricky Argenbright <honorable.anomaly@gmail.com>
2022-09-11 15:38:28 -0700

    Create Repository class + Add styles
    app.js
    +require and use products router
    
    repositories\repository.js
    +copy reusable repository code from 'users.js'
    
    users.js
    +require 'Repository' class and extend 'UsersRepository'
    
    routes\admin\products.js
    +require express and router
    +'products' and new product get routes
    +module.exports for router
    
    views\admin\auth\signin.js
    +add HTML for layout and styling
    
    views\admin\auth\signup.js
    +add HTML for layout and styling
    
    views\admin\layout.js
    +add HTML for layout and styling

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-Parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
+const productsRouter = require("./routes/admin/products");
 
 const app = express();
 
+app.use(express.static("public"));
 // Necessary unless we want to paste this bodyParser line on every route
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -14,6 +16,7 @@ app.use(
 );
 
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3333, () => {
   console.log(">>> Listening on port 3333... <<<");
--- repositories/users.js
+++ repositories/users.js
@@ -1,25 +1,11 @@
 const fs = require("fs");
 const crypto = require("crypto");
 const util = require("util");
+const Repository = require("./repository");
 
 const scrypt = util.promisify(crypto.scrypt);
 
-class UsersRepository {
-  constructor(filename) {
-    if (!filename) {
-      throw new Error("Creating a repository requires a filename");
-    }
-
-    this.filename = filename;
-    try {
-      fs.accessSync(this.filename);
-    } catch (err) {
-      fs.writeFileSync(this.filename, "[]");
-    }
-  }
-  async getAll() {
-    return JSON.parse(await fs.promises.readFile(this.filename, { encoding: "utf8" }));
-  }
+class UsersRepository extends Repository {
   async create(stats) {
     stats.id = this.randomId();
 
@@ -29,7 +15,7 @@ class UsersRepository {
     const records = await this.getAll();
     const record = {
       ...stats,
-      password: `${buffer.toString("hex")}.${salt}`,
+      password: `${buffer.toString("hex")}.${salt}`
     };
     records.push(record);
 
@@ -42,47 +28,6 @@ class UsersRepository {
     const hashedSuppliedBuffer = await scrypt(supplied, salt, 64);
 
     return hashed === hashedSuppliedBuffer.toString("hex");
-  }
-  async writeAll(records) {
-    await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
-  }
-  randomId() {
-    return crypto.randomBytes(4).toString("hex");
-  }
-  async getOne(id) {
-    const records = await this.getAll();
-    return records.find((record) => record.id === id);
-  }
-  async delete(id) {
-    const records = await this.getAll();
-    const filteredRecords = records.filter((record) => record.id !== id);
-    await this.writeAll(filteredRecords);
-  }
-  async update(id, stats) {
-    const records = await this.getAll();
-    const record = records.find((record) => record.id === id);
-
-    if (!record) {
-      throw new Error(`Record ${id} not found`);
-    }
-
-    Object.assign(record, stats);
-    await this.writeAll(records);
-  }
-  async getOneBy(filters) {
-    const records = await this.getAll();
-    for (let record of records) {
-      let found = true;
-
-      for (let key in filters) {
-        if (record[key] !== filters[key]) {
-          found = false;
-        }
-      }
-      if (found) {
-        return record;
-      }
-    }
   }
 }
 
