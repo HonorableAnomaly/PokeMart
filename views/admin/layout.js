c9aba8f7438285a117b69625d467b2ba63d40c71
Ricky Argenbright <honorable.anomaly@gmail.com>
2022-09-13 19:36:01 -0700

    Add seed products + Add tab icon
    products.json
    +9 seed products added
    
    views\admin\layout.js
    +add link for tab icon

--- views/admin/layout.js
+++ views/admin/layout.js
@@ -5,7 +5,8 @@ module.exports = ({ content }) => {
         <head>
           <meta charset="utf-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Shop</title>
+          <link rel="icon" href="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c861d306-8f42-4864-ab2e-61a271518c8a/df6euho-43e72823-7377-4bd3-9b60-ef1d7bd05966.png/v1/fit/w_600,h_1281,strp/gold_ball_by_ace_zeroartic_df6euho-300w-2x.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MSIsInBhdGgiOiJcL2ZcL2M4NjFkMzA2LThmNDItNDg2NC1hYjJlLTYxYTI3MTUxOGM4YVwvZGY2ZXVoby00M2U3MjgyMy03Mzc3LTRiZDMtOWI2MC1lZjFkN2JkMDU5NjYucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.0sNTQIule0HQkZQdpZ-j852VnnsDYJW9umNBZnzCGog" />
+          <title>Pokémart</title>
           <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
           <link href="/css/main.css" rel="stylesheet">
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css"></link>
