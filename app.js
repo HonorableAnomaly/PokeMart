d811264ca26cf6552d5b53b72d2ee3f66cf87828
Ricky Argenbright <honorable.anomaly@gmail.com>
2022-09-15 12:25:25 -0700

    Add products customer view
    app.js
    +require files and use
    
    routes\products.js
    +requires for express, productsRepo and productsIndexTemplate
    +get route for root view to display main shop page
    
    view\layout.js
    +module.exports to render content HTML for main products view
    
    views\products\index.js
    +require layout
    +module.exports to render products HTML within the layout view

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-Parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
-const productsRouter = require("./routes/admin/products");
+const adminProductsRouter = require("./routes/admin/products");
+const productsRouter = require("./routes/products");
 
 const app = express();
 
@@ -11,11 +12,12 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
-    keys: ["thisisahorriblekey"]
+    keys: ["thisisahorriblekey"],
   })
 );
 
 app.use(authRouter);
+app.use(adminProductsRouter);
 app.use(productsRouter);
 
 app.listen(3333, () => {
---
+++ views/layout.js
@@ -0,0 +1,66 @@
+module.exports = ({ content }) => {
+  return `
+    <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c861d306-8f42-4864-ab2e-61a271518c8a/df6euho-43e72823-7377-4bd3-9b60-ef1d7bd05966.png/v1/fit/w_600,h_1281,strp/gold_ball_by_ace_zeroartic_df6euho-300w-2x.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MSIsInBhdGgiOiJcL2ZcL2M4NjFkMzA2LThmNDItNDg2NC1hYjJlLTYxYTI3MTUxOGM4YVwvZGY2ZXVoby00M2U3MjgyMy03Mzc3LTRiZDMtOWI2MC1lZjFkN2JkMDU5NjYucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.0sNTQIule0HQkZQdpZ-j852VnnsDYJW9umNBZnzCGog" />
+        <title>Pokémart</title>
+        
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
+        <link href="/css/main.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css"></link>
+      </head>
+
+      <body>
+        <header>
+          <nav class="navbar navbar-top">
+            <div class="container navbar-container">
+              <div>
+                <ul class="social">
+                  <li>
+                    <a href=""><i class="fa fa-phone"></i>+1 333 987 6543</a>
+                  </li>
+                  <li>
+                    <a href=""><i class="fa fa-envelope"></i> pokemart@pokemart.com</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <ul class="social">
+                  <li><a href=""><i class="fab fa-facebook"></i></a></li>
+                  <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                  <li><a href=""><i class="fab fa-linkedin"></i></a></li>
+                  <li><a href=""><i class="fab fa-dribbble"></i></a></li>
+                  <li><a href=""><i class="fab fa-google-plus"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <nav class="navbar navbar-bottom">
+            <div class="container navbar-container">
+              <div>
+                <a href="/">
+                  <h3 class="title"><img class="shop-logo" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c861d306-8f42-4864-ab2e-61a271518c8a/df6euho-43e72823-7377-4bd3-9b60-ef1d7bd05966.png/v1/fill/w_1280,h_1281,strp/gold_ball_by_ace_zeroartic_df6euho-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MSIsInBhdGgiOiJcL2ZcL2M4NjFkMzA2LThmNDItNDg2NC1hYjJlLTYxYTI3MTUxOGM4YVwvZGY2ZXVoby00M2U3MjgyMy03Mzc3LTRiZDMtOWI2MC1lZjFkN2JkMDU5NjYucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.0sNTQIule0HQkZQdpZ-j852VnnsDYJW9umNBZnzCGog" /> Pokémart</h3>
+                </a>
+              </div>
+              <div class="navbar-item">
+                <div class="navbar-buttons">
+                  <div class="navbar-item">
+                    <a href="/"><i class="fa fa-star"></i> Products</a>
+                  </div>
+                  <div class="navbar-item">
+                    <a href="/cart"><i class="fa fa-shopping-cart"></i> Cart</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </header>
+
+        ${content}
+      </body>
+    </html>
+  `;
+};
