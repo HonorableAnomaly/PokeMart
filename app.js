1a91649a550083697903613cba0e77deded3df3a
Ricky Argenbright <honorable.anomaly@gmail.com>
2022-09-15 21:35:57 -0700

    Add and style cart feature
    routes\carts.js
    +post route for /cart/products that creates a cart for a user if there isn't one and then adds a product if it isn't in the cart if it exists, and adds to the quantity if it does exist
    +get route for /cart that renders the cart template and all products
    +post route for /cart/products/delete that removes a product from the cart
    
    views\carts\show.js
    module.exports with items that calculates totalPrice of cart and renders the HTML for each item as it's mapped, along with the layout template
    
    views\products\index.js
    +add hidden input to detect the product.id of the item added to the cart

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const adminProductsRouter = require("./routes/admin/products");
 const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
 
 const app = express();
 
@@ -12,13 +13,14 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
-    keys: ["thisisahorriblekey"],
+    keys: ["thisisahorriblekey"]
   })
 );
 
 app.use(authRouter);
 app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
 
 app.listen(3333, () => {
   console.log(">>> Listening on port 3333... <<<");
